077560e28751f51b33f7ca39515562de5dc4aed9
bruks <jenake8@gmail.com>
2022-07-24 16:48:37 +0100

    list response to string

--- apps/api/renderers.py
+++ apps/api/renderers.py
@@ -3,7 +3,6 @@ from rest_framework.renderers import JSONRenderer
 class CustomRenderer(JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
         status_code = renderer_context['response'].status_code
-        print(status_code)
         response = {
           "status": "success",
           "code": status_code,
@@ -18,16 +17,15 @@ class CustomRenderer(JSONRenderer):
                 response["message"] = data["detail"]
             except KeyError:
                 response["data"] = data
-            data["details"] = []
+            data["details"] = ""
             for i in data:
                 if i != "details":
                     if data[i][0] == "This field must be unique.":
-                        data["details"].append(f"{i}: This {i} is already taken")
+                        data["details"] += f"{i}: This {i} is already taken\n"
                     else:
-                        data["details"].append(f"{i}: {data[i][0]}")
+                        data["details"] += f"{i}: {data[i][0]}\n"
             for i in list(data.keys()):
                 if i != "details":
                     del data[i]
-
 
         return super(CustomRenderer, self).render(data, accepted_media_type, renderer_context)
