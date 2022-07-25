4495d581db0002e3dfc5193a8eb5adaed251ac5e
bruks <jenake8@gmail.com>
2022-07-25 16:39:42 +0100

    custom renderers fixed

--- apps/api/renderers.py
+++ apps/api/renderers.py
@@ -29,29 +29,3 @@ class CustomRenderer(JSONRenderer):
                     del data[i]
 
         return super(CustomRenderer, self).render(data, accepted_media_type, renderer_context)
-
-class CustomeLoginRenderer(JSONRenderer):
-    def render(self, data, accepted_media_type=None, renderer_context=None):
-        status_code = renderer_context['response'].status_code
-        response = {
-          "status": "success",
-          "code": status_code,
-          "data": data,
-          "message": None
-        }
-        if not str(status_code).startswith('2'):
-            response["status"] = "error"
-            response["data"] = None
-            try:
-                response["message"] = data["detail"]
-            except KeyError:
-                response["data"] = data
-            data["details"] = ""
-            for i in data:
-                if i != "details":
-                    data["details"] += f"{i}: {data[i][0]}\n"
-            for i in list(data.keys()):
-                if i != "details":
-                    del data[i]
-
-        return super(CustomeLoginRenderer, self).render(data, accepted_media_type, renderer_context)
--- apps/auth/views.py
+++ apps/auth/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from .serializers import MyTokenObtainPairSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
-from api.renderers import CustomRenderer, CustomeLoginRenderer
+from api.renderers import CustomRenderer
 
 # Create your views here.
 class MyTokenObtainPairView(TokenObtainPairView):
     permission_classes = (AllowAny,)
     serializer_class = MyTokenObtainPairSerializer
-    renderer_classes = (CustomeLoginRenderer,)
+    renderer_classes = (CustomRenderer,)
