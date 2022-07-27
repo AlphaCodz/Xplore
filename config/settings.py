c726a8ae1ce5c0c94dcee0cb6a093d903721f55b
AlphaCodz <adeyanjuabiona69@gmail.com>
2022-07-27 10:27:18 +0100

    my postgreupdate

--- config/settings.py
+++ config/settings.py
@@ -89,9 +89,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'explore', 
         'USER': 'postgres', 
-        'PASSWORD': 'Bruks888,',
+        'PASSWORD': 'ALeader102030,',
         'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'PORT': '5433',
     }
 }
 
