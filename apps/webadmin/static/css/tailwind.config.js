4dbd1e7eb8090e56de54faa5fb93a4052e2b4101
AlphaCodz <adeyanjuabiona69@gmail.com>
2022-07-27 11:36:52 +0100

    WebAPP added

---
+++ apps/webadmin/static/css/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dist/index.html",
+    "./dist/pending.html",
+    "./dist/approved.html",
+    "./dist/declined.html",
+    "./dist/paid.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -28,6 +28,7 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('api/admin/', include('admin_app.urls')),
     path('api/tours/', include('tours.urls')),
+    re_path(r'^web/', include('webadmin.urls')),
 ]
 
 if settings.DEBUG:
