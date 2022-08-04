cb4199c7ba886b1addf88ae55105fa9c21afcc0f
AlphaCodz <adeyanjuabiona69@gmail.com>
2022-08-05 00:57:59 +0100

    adminpanelAPIadded

--- apps/admin_app/views.py
+++ apps/admin_app/views.py
@@ -1,11 +1,13 @@
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from api.models import Customer
 from api.serializers import CustomerSerializer
 from rest_framework import generics, permissions
-from django.views.generic import (View, TemplateView, ListView, DetailView, 
-                                  CreateView, UpdateView, DeleteView)
-
+from rest_framework.decorators import permission_classes, api_view, authentication_classes
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from tours.models import *
+from tours.serializers import BookingSerializer, UserDetailSerializer
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 class CustomerList(generics.ListAPIView):
@@ -22,7 +24,49 @@ class CustomerList(generics.ListAPIView):
         print(start, stop)
         qs = Customer.objects.all()[start:stop]
         return qs
+    
+@api_view(["GET"])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([permissions.IsAdminUser])
+def detail_counts(request):
+    
+        pending = Booking.objects.filter(status="P").count()
+        approved = Booking.objects.filter(status="A").count()
+        declined = Booking.objects.filter(status="D").count()
+        paid = Booking.objects.filter(paid = True).count()
+        
+        # Passing queries as context
+        query = {
+        "pending": pending,
+        "approved": approved,
+        "declined": declined,
+        "paid": paid,
+    }
+    
+        return JsonResponse(query)
 
+def UserDetailsList(request, id):
+   user = Customer.objects.get(id=id)
+   details = Booking.objects.all()
+   detail_list = []
+   for details in details:
+        query = {
+       "user_id":details.customer.id,
+       "Booking_id":details.id,
+       "category": details.category,
+       "first_name":details.customer.first_name,
+       "last_name":details.customer.last_name,
+       "email":details.customer.email,
+       "package_type":details.package.package_type,
+       "package_price": str(details.package.price),
+       "number_of_person": details.individuals
+            }
+        
+        detail_list.append(query)
+        context_data = {"detail_list":detail_list}
+            
+        return JsonResponse(context_data)
 
+    
 
 
--- apps/tours/serializers.py
+++ apps/tours/serializers.py
@@ -10,12 +10,12 @@ class BookingSerializer(serializers.ModelSerializer):
     agent = serializers.StringRelatedField()
     tour = serializers.StringRelatedField()
     package = serializers.StringRelatedField()
-    visa = serializers.StringRelatedField(many=True, read_only=True)
+    # visa = serializers.StringRelatedField(many=True, read_only=True)
     passports = serializers.StringRelatedField(many=True)
     customer = serializers.StringRelatedField()
     class Meta:
         model = Booking
-        fields = ("id", "customer", "category", "agent", "tour", "package", "individuals", "status", "paid", "visa", "passports")
+        fields = ("id", "customer", "category", "agent", "tour", "package", "individuals", "status", "paid", "passports")
         extra_kwargs = {
             'tour': {'required': False},
             'agent': {'required': False},
@@ -37,21 +37,29 @@ class BookingSerializer(serializers.ModelSerializer):
             individuals = validated_data.get("individuals")
         )
         booking.save()
-        files = request.FILES
-        expected_passport_files = ["passport1", "passport2", "passport3"]
-        expected_visa_files = ["visa1", "visa2", "visa3"]
-        for i in expected_passport_files:
-            if files.get(i):
-                passport = Passport.objects.create(
-                    booking = booking,
-                    image = files.get(i),
-                )
-                passport.save()
-        for i in expected_visa_files:
-            if files.get(i):
-                visa = Visa.objects.create(
-                    image = files.get(i),
-                    booking = booking,
-                )
-                visa.save()
-        return booking
+        # files = request.FILES
+        # expected_passport_files = ["passport1", "passport2", "passport3"]
+        # expected_visa_files = ["visa1", "visa2", "visa3"]
+        # for i in expected_passport_files:
+        #     if files.get(i):
+        #         passport = Passport.objects.create(
+        #             booking = booking,
+        #             image = files.get(i),
+        #         )
+        #         passport.save()
+        # for i in expected_visa_files:
+        #     if files.get(i):
+        #         visa = Visa.objects.create(
+        #             image = files.get(i),
+        #             booking = booking,
+        #         )
+        #         visa.save()
+        # return booking
+        
+class UserDetailSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Booking
+        fields = ("customer_id", "customer", "category", "no_of_persons_on_tour","payment_reference", "package", "email", "package_type")
+        
+    
