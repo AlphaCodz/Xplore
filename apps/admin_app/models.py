280d0a2f089026616c0007484680c085b12a5227
AlphaCodz <adeyanjuabiona69@gmail.com>
2022-08-06 21:58:22 +0100

    admin model updated

--- apps/admin_app/models.py
+++ apps/admin_app/models.py
@@ -1,18 +1,22 @@
+import email
 from django.db import models
 from birthday import BirthdayField, BirthdayManager
+from api.models import Customer
 
 # Create your models here.
 class AdminReg(models.Model):
-    first_name = models.CharField(max_length=250)
-    last_name = models.CharField(max_length=250)
-    email = models.EmailField(unique=True)
-    staff_no = models.CharField(max_length=6, unique=True)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, null=True)
+
+    #proxy fields
+    email = models.EmailField(unique=True, null=True)
+    first_name = models.CharField(max_length=250, null=True)
+    last_name = models.CharField(max_length=250, null=True)
+
+    staff_number = models.CharField(max_length=6, unique=True)
     birthday = BirthdayField(null=True)
-    home_address = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    password2 = models.CharField(max_length=200)
-    
+    home_address = models.CharField(max_length=200, null=True)
+
     objects = BirthdayManager()
     
     def __str__(self):
-        return f"{self.last_name} {self.staff_no}"
+        return f"{self.last_name} {self.staff_number}"
