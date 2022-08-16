1132c9f413bb93512f8b54775dbf8ecd166fdaa1
bruks <jenake8@gmail.com>
2022-08-16 11:15:33 +0100

    fixed email url

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -4,6 +4,6 @@ from .views import RegisterCustomer, CustomerDetail, GenerateToken, verify_email
 urlpatterns = [
     path('register', RegisterCustomer.as_view(), name='register'),
     path('me', CustomerDetail.as_view(), name="me"),
-    path('send_email_reset_token', GenerateToken.as_view(), name='generate-token'),
+    path('send_email_verification_token', GenerateToken.as_view(), name='generate-token'),
     path('verify_email/<token>', verify_email, name='verify-email'),
 ]
