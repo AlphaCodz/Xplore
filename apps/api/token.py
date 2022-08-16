110128ad9a20a05b08b9af8ce74487a17e9e49df
bruks <jenake8@gmail.com>
2022-08-16 10:55:07 +0100

    email verification

--- apps/api/token.py
+++ apps/api/token.py
@@ -1,1 +1,33 @@
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from config.settings import SECRET_KEY
+from datetime import datetime, timedelta
+import jwt
+
+def generate_token_from_user(user):
+    user["expires"] = str(datetime.now() + timedelta(minutes=5))
+    print(user)
+    token = jwt.encode(user, SECRET_KEY, "HS256")
+    return token
+
+def validate_token(token):
+    try:
+        user = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        expires = datetime.strptime(user["expires"], '%Y-%m-%d %H:%M:%S.%f')
+        difference = datetime.now() - expires
+        if difference > timedelta(minutes=5):
+            return {
+                "status": False,
+                "message": "Expired token"
+            }
+        else:
+            data = {
+                "status": True,
+                "message": "Your email has been verified",
+                "user": user,
+            }
+            return data
+    except:
+        data = {
+            "status": False,
+            "message": "Invalid Token"
+        }
+        return data
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,10 +1,13 @@
-from urllib.parse import uses_params
+import email
 from .serializers import CustomerSerializer, RegisterSerilizer
 from rest_framework import generics, permissions
 from .models import Customer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from api.renderers import CustomRenderer
+from .token import generate_token_from_user, validate_token
+from django.core.mail import send_mail
+from django.http.response import HttpResponse
 
 # Create your views here.
 class CustomerList(generics.ListAPIView):
@@ -35,3 +38,27 @@ class CustomerDetail(APIView):
         if request.user:
             serializer = CustomerSerializer(user)
             return Response(serializer.data)
+
+class GenerateToken(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    def post(self, request):
+        host = request.get_host()
+        user = request.user
+        data = {}
+        data["email"] = user.email
+        token = generate_token_from_user(data)
+        send_mail(
+            "Verify Email",
+            f"http://{host}/api/verify_email/{token}",
+            "bbruks07@gmail.com",
+            [user.email],
+        )
+        return Response({"message": "verification mail sent"})
+
+def verify_email(request, token):
+    validation = validate_token(token)
+    if validation["status"]:
+        user = Customer.objects.get(email=validation["user"]["email"])
+        user.verified_email = True
+        user.save()
+    return HttpResponse(validation["message"])
