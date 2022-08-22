02442548d6623a03757effe5aa595760324440bb
bruks <jenake8@gmail.com>
2022-08-22 16:05:51 +0100

    refactor angency views

--- apps/touragency/views.py
+++ apps/touragency/views.py
@@ -1,25 +1,17 @@
-from math import perm
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions
 from api.models import Customer
 from .models import TourAgency
 from django.http import JsonResponse
-from .serializers import TourAgencySerializer, MyTokenObtainPairSerializer
 from rest_framework import generics, status
-from rest_framework_simplejwt.views import TokenObtainPairView
-from tours.models import Tour, Agent, Booking
-from tours.serializers import TourSerializer
-from rest_framework.views import APIView
+from tours.models import Tour, Booking
 from tours.serializers import TourSerializer, BookingSerializer
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
-from rest_framework import serializers
+from .serializers import TourAgencySerializer
 
-# Create your views here.
-class MyTokenObtainPairView(TokenObtainPairView):
-    serializer_class = MyTokenObtainPairSerializer
-    
-    
+# Create your views here.   
 class RegisterTourAgency(generics.GenericAPIView):
     serializer_class = TourAgencySerializer
     
@@ -32,10 +24,7 @@ class RegisterTourAgency(generics.GenericAPIView):
         
         return Response(agency_data, status=status.HTTP_201_CREATED)
     
-
-
 @api_view(["GET"])
-@permission_classes([permissions.IsAuthenticated])
 @permission_classes([permissions.IsAdminUser])
 def AgencyDetails(request, id):
     user = Customer.objects.get(id=id)
@@ -62,19 +51,12 @@ class AddTour(generics.CreateAPIView):
     
     # To view all the Tour Lists no matter the id
 class TourList(APIView):
-    """
-    List all snippets, or create a new snippet.
-    """
     def get(self, request, format=None):
         snippets = Tour.objects.all()
         serializer = TourSerializer(snippets, many=True)
         return Response(serializer.data)
 
 class TourDetail(APIView):
-    """
-   get, update or delete a tour.
-   Note that the get works by id so id must be specified
-    """
     def get_object(self, pk):
         try:
             return Tour.objects.get(pk=pk)
@@ -105,11 +87,10 @@ def all_bookings(request, status):
     bookings = []
     for booking in qs:
         json_form = {
-            "booking_id": booking.id,
+            "id": booking.id,
             "customer": str(booking.customer),
-            "Assigned Tour Agent": str(booking.agent),
-            "From": str(booking.agent.tour_agency)
-
+            "agent": str(booking.agent),
+            "from": str(booking.agent.tour_agency)
         }
         bookings.append(json_form)
     data = {status:bookings}
