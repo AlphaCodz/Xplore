cbf9926cd7f31de2219b88ae3b1bdd85f7fb2435
bruks <jenake8@gmail.com>
2022-08-22 16:11:21 +0100

    refactor angency urls

--- apps/touragency/urls.py
+++ apps/touragency/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, re_path
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import RegisterTourAgency, TokenObtainPairView, AddTour, TourDetail, all_bookings, approve_booking
+from .views import RegisterTourAgency, AddTour, TourDetail, approve_booking
 
 urlpatterns = [
     path('<int:id>', views.AgencyDetails, name='agencies'),
     path('touragency/register', RegisterTourAgency.as_view(), name="regtouragency"),
-    path('login', TokenObtainPairView.as_view(), name="agencylogin"),
     path('add/tour', AddTour.as_view(), name="addtour"),
     path('tours/<int:pk>', TourDetail.as_view(), name="tours" ),    
     path('all_bookings/<status>', views.all_bookings, name="bookings"),
