c36d78dc6d075a0db786df24d6500a6f2cd70539
bruks <jenake8@gmail.com>
2022-08-24 18:38:15 +0100

    retrieve admin

--- apps/tours/admin.py
+++ apps/tours/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Tour, Agent, Booking, TourAgency
+from .models import Tour, Agent, Booking, Package
 
 
 # Register your models here.
 admin.site.register(Tour)
 admin.site.register(Agent)
-# admin.site.register(Admin)
+admin.site.register(Package)
 admin.site.register(Booking)
-
+admin.site.register(Booking)
